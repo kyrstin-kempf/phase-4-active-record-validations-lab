45964c681a4f6f9cb89dcc7f0f6a5e9be4b558e6
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-10-18 23:04:13 -0400

    commit tree 9bd109c06ac85180aa806586258a11bbbaa66ecf

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :clickbait_y_title
+
+    ACCEPTED_TITLES = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/
+    ]
+
+    def clickbait_y_title
+        if ACCEPTED_TITLES.none? { |words| words.match title }
+            errors.add( :title, "Add clickbait words")
+        end
+    end
+
 end
